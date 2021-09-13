d519333807fe93847c6feee80bc7807ce1087bdd
Zane Lu <zlu@logitech.com>
2021-09-13 14:09:26 +0800

    Handle the multi-monitors issue

--- ProcessInspector/ProcessMemInfoDlg.cpp
+++ ProcessInspector/ProcessMemInfoDlg.cpp
@@ -20,6 +20,78 @@ const int SIZE_COLUMN = 4;
 const int BASE_COLUMN = 5;
 
 
+
+
+#define MONITOR_CENTER   0x0001        // center rect to monitor
+#define MONITOR_CLIP     0x0000        // clip rect to monitor
+#define MONITOR_WORKAREA 0x0002        // use monitor work area
+#define MONITOR_AREA     0x0000        // use monitor entire area
+
+//
+//  ClipOrCenterRectToMonitor
+//
+//  The most common problem apps have when running on a
+//  multimonitor system is that they "clip" or "pin" windows
+//  based on the SM_CXSCREEN and SM_CYSCREEN system metrics.
+//  Because of app compatibility reasons these system metrics
+//  return the size of the primary monitor.
+//
+//  This shows how you use the multi-monitor functions
+//  to do the same thing.
+//
+void ClipOrCenterRectToMonitor(LPRECT prc, UINT flags)
+{
+    HMONITOR hMonitor;
+    MONITORINFO mi;
+    RECT        rc;
+    int         w = prc->right - prc->left;
+    int         h = prc->bottom - prc->top;
+
+    //
+    // get the nearest monitor to the passed rect.
+    //
+    hMonitor = MonitorFromRect(prc, MONITOR_DEFAULTTONEAREST);
+
+    //
+    // get the work area or entire monitor rect.
+    //
+    mi.cbSize = sizeof(mi);
+    GetMonitorInfo(hMonitor, &mi);
+
+    if (flags & MONITOR_WORKAREA)
+        rc = mi.rcWork;
+    else
+        rc = mi.rcMonitor;
+
+    //
+    // center or clip the passed rect to the monitor rect
+    //
+    if (flags & MONITOR_CENTER)
+    {
+        prc->left = rc.left + (rc.right - rc.left - w) / 2;
+        prc->top = rc.top + (rc.bottom - rc.top - h) / 2;
+        prc->right = prc->left + w;
+        prc->bottom = prc->top + h;
+    }
+    else
+    {
+        prc->left = max(rc.left, min(rc.right - w, prc->left));
+        prc->top = max(rc.top, min(rc.bottom - h, prc->top));
+        prc->right = prc->left + w;
+        prc->bottom = prc->top + h;
+    }
+}
+
+void ClipOrCenterWindowToMonitor(HWND hwnd, UINT flags)
+{
+    RECT rc;
+    GetWindowRect(hwnd, &rc);
+    ClipOrCenterRectToMonitor(&rc, flags);
+    SetWindowPos(hwnd, NULL, rc.left, rc.top, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_NOACTIVATE);
+}
+
+
+
 template <class InIter1, class InIter2>
 void find_all(unsigned char *base, InIter1 buf_start, InIter1 buf_end, InIter2 pat_start, InIter2 pat_end) {
     for (InIter1 pos = buf_start;
@@ -356,6 +428,8 @@ BOOL CProcessMemInfoDlg::OnInitDialog()
     BEGIN_OBJ_MAP(CProcessMemInfoDlg);
     OBJ_DEFINE(IDC_MEM_INFO_LIST, 0, 100, 0, 100);
     END_OBJ_MAP();
+
+    ClipOrCenterWindowToMonitor(GetSafeHwnd(), MONITOR_CLIP);
 
 
     /* Testing
